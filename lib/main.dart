1b0233b16db2a8704232e652c95ecbde72b59f68
Zunderene <hector21450@gmail.com>
2021-08-28 10:48:48 +0200

    Change title Ingles Demo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ingles Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.blue,
